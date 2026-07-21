89174d7a258baa415a3d607504a1e2036431a395
ajadeshwarsharma-ctrl <ajadeshwarsharma@gmail.com>
2026-07-21 15:24:09 +0530

    Update indexer.js

--- indexer.js
+++ indexer.js
@@ -2,3 +2,28 @@ console.log("🤖 AJADESH INDEXER STARTED");
 const fs = require("fs");
 
 console.log("📚 Loading pages database...");
+let pages = [];
+
+try {
+
+    pages = JSON.parse(
+        fs.readFileSync(
+            "data.json",
+            "utf8"
+        )
+    );
+
+    console.log(
+        "✅ Pages Loaded:",
+        pages.length
+    );
+
+}
+
+catch (error) {
+
+    console.log(
+        "❌ data.json nahi mila ya empty hai."
+    );
+
+}
