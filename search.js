5553c97679d641b5c39d3fac74fdcf5cd97542e2
ajadeshwarsharma-ctrl <ajadeshwarsharma@gmail.com>
2026-07-22 19:17:48 +0530

    Update search.js

--- search.js
+++ search.js
@@ -1,6 +1,6 @@
 const fs = require("fs");
 const { rankResults } = require("./ranker");
-
+const { getSuggestions } = require("./suggestions");
 function search(query) {
 
     const index = JSON.parse(
